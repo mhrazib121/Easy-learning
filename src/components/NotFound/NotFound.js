f95ee1b7cd81477e50d15a570484e2a68dd4194b
mhrazib121 <mhrazib121@gmail.com>
2021-10-05 21:17:45 +0600

    some edited

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div className="text-center">
-            <img className="w-25 p-4" src="https://www.computerhope.com/jargon/e/error.png" alt="" />
+        <div className="text-center error-position">
+            <img className="w-25 p-4 " src="https://www.computerhope.com/jargon/e/error.png" alt="" />
             <h1> <span className="text-danger"> 404 </span> Page Not Found</h1>
             <p>The requested page could not be found, Please try again...</p>
         </div>
